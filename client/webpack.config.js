eceaff5beeea4b10c295e7d494a88e2c5ac197c2
Chema Rivera <chema22r@gmail.com>
2019-06-26 23:24:48 +0200

    Three.js package was added into the package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,7 +34,8 @@ module.exports = {
         ]),
         new webpack.ProvidePlugin({
             $: "jquery",
-            jQuery: "jquery"
+            jQuery: "jquery",
+            THREE: "three"
         })
     ]
 };
