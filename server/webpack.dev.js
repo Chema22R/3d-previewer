40f6385a1eb80a9404555aa0bf7699149ad3dd19
Chema <chema22r@gmail.com>
2019-09-04 21:02:06 +0200

    Nodemon-webpack module implemented and some npm scripts modified

--- server/webpack.dev.js
+++ server/webpack.dev.js
@@ -2,6 +2,7 @@ const common = require("./webpack.config.js");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const NodemonPlugin = require("nodemon-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
@@ -11,6 +12,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             DEFAULT_PORT: 8000,
             DEFAULT_DATABASE_URI: JSON.stringify("mongodb://localhost/3dpreviewer")
-        })
+        }),
+        new NodemonPlugin()
     ]
 });
