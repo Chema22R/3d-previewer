a90d6d9fe06671ee891670a065ddac278f9576a6
Rivera Alonso <jose.maria.rivera.alonso@hp.com>
2019-09-11 14:01:45 +0200

    Added a second attempt to retrieve file list

--- client/src/app/fileList.js
+++ client/src/app/fileList.js
@@ -9,11 +9,19 @@ $(function() {
 
         $(".loadingBar.wrapper").fadeIn("fast");
 
+        getFileList(0);
+    });
+
+    function getFileList(attempts) {
         $.ajax({
             url: SERVER_URL+"/file",
             method: "GET",
             success: function(res, status) {
-                entriesGenerator(res);
+                if (res.length === 0 && attempts === 0) {
+                    getFileList(++attempts);
+                } else {
+                    entriesGenerator(res);
+                }
             },
             error: function(jqXHR, status, err) {
                 if (!err) {
@@ -23,7 +31,7 @@ $(function() {
                 }
             }
         });
-    });
+    }
 
     function entriesGenerator(res) {
         if (res.length == 0) {
@@ -43,15 +51,13 @@ $(function() {
             }
         }
 
-        setTimeout(() => {
-            $(".loadingBar.wrapper").fadeOut("fast");
-            $(".fileList.menu").fadeIn("slow");
-            $(".fileList.menu").scrollTop(0);
-        }, 100);
+        $(".loadingBar.wrapper").fadeOut("fast");
+        $(".fileList.menu").fadeIn("slow");
 
         setTimeout(() => {
+            $(".fileList.menu").scrollTop(0);
             delayedLoad();
-        }, 120);
+        }, 10);
     }
 
 
